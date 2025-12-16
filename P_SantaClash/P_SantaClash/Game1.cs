5c3be8bd9212594a0832040421466afc7e5af8ae
Leartdemiri <leart.dmr2@eduge.ch>
2025-12-16 13:34:54 +0100

    Added black santa

--- P_SantaClash/P_SantaClash/Game1.cs
+++ P_SantaClash/P_SantaClash/Game1.cs
@@ -9,6 +9,11 @@ namespace P_SantaClash
         private GraphicsDeviceManager _graphics;
         private SpriteBatch _spriteBatch;
 
+        Texture2D background;
+        Texture2D santaTexture;
+        Santa santa;
+
+
         public Game1()
         {
             _graphics = new GraphicsDeviceManager(this);
@@ -26,6 +31,11 @@ namespace P_SantaClash
         protected override void LoadContent()
         {
             _spriteBatch = new SpriteBatch(GraphicsDevice);
+            background = Content.Load<Texture2D>("wallpaper");
+            santaTexture = Content.Load<Texture2D>("black_santa");
+            Vector2 santaPosition = new Vector2(GraphicsDevice.Viewport.Width / 2 - santaTexture.Width/2, GraphicsDevice.Viewport.Height / 2 - santaTexture.Height/2);
+            santa = new Santa(santaPosition, 100, santaTexture);
+
 
             // TODO: use this.Content to load your game content here
         }
@@ -45,6 +55,17 @@ namespace P_SantaClash
             GraphicsDevice.Clear(Color.CornflowerBlue);
 
             // TODO: Add your drawing code here
+
+            // basically, rectangle that starts from the corner that ends to the other corner which is the size of the viewport 
+            Rectangle wallpaperSize = new Rectangle(0,0,GraphicsDevice.Viewport.Width,GraphicsDevice.Viewport.Height);
+
+
+
+
+            _spriteBatch.Begin();
+            _spriteBatch.Draw(background, wallpaperSize, Color.White);
+            santa.Draw(_spriteBatch);
+            _spriteBatch.End();
 
             base.Draw(gameTime);
         }
---
+++ P_SantaClash/P_SantaClash/Santa.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+
+namespace P_SantaClash
+{
+    public class Santa
+    {
+        public Vector2 Position { get; set; }
+        public int MaxHealth { get; set; }
+        public int CurrentHealth { get; set; }
+        public Texture2D texture;
+
+        // => so it uses actual class stuff and (int) to convert from float to int 
+        public Rectangle Hitbox => new Rectangle((int)Position.X, (int)Position.Y, texture.Width, texture.Height); // position of the santa to the size of the texture of santa
+
+
+        public Santa(Vector2 position, int maxHealth, Texture2D texture)
+        {
+            Position = position;
+            MaxHealth = maxHealth;
+            CurrentHealth = maxHealth;
+            this.texture = texture;
+        }
+
+        public void TakeDamage(int dmg){
+
+            if(this.CurrentHealth <= 0){
+                this.CurrentHealth = 0; // so that it doesnt show bugged
+                return;
+            }else{
+                this.CurrentHealth -= dmg;
+            }
+        }
+
+        public void Draw(SpriteBatch spriteBatch)
+        {
+            if(this.CurrentHealth <= 0){
+                return; // so that it doesnt draw if dead
+            }
+
+            spriteBatch.Draw(texture, Position, Color.White);
+        }
+    }
+}
